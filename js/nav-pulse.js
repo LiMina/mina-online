5c9195979e73c108a6ab0605101c3e226838f31f
Mina Li <li.mina888@berkeley.edu>
2014-08-30 20:51:54 -0700

    Finishing off the Code section of the website.

--- js/nav-pulse.js
+++ js/nav-pulse.js
@@ -1,6 +1,6 @@
 $( document ).ready( function() {
 	$( ".pulse").click( function() {
-		$(this).animate({height: "+=20px"}, "slow").addClass("select");
 		$(this).siblings(".select").animate({height: "-=20px"}, "slow").removeClass("select");
+		$(this).animate({height: "+=20px"}, "slow").addClass("select");
 	});
 });
